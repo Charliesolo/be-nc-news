7a044a43e61d71122f4c8385b0a6f8a9b3d24d05
Charliesolo <charliesolomon99@googlemail.com>
2024-10-16 11:42:02 +0100

    start preparing repo for hosting

--- listen.js
+++ listen.js
@@ -1,7 +1,6 @@
 const app = require('./app')
 
+const { PORT = 8080} = process.env
 
-app.listen(8080, (err) => {
-    if (err) console.log(err);
-    else console.log("listening on 8080");
-});
+
+app.listen( PORT, () => {`Listening on ${PORT}...`});
